2fc10655e9d09c8b36e1e1eb8aebe416dd06dd1d
sangukO <woqls1972@gmail.com>
2026-01-22 17:56:37 +0900

    feat: Tanstack Query 설정 및 App 레이어 Provider 주입

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// tanstack query
+// 인스턴스 생성
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // 캐싱 시간 설정
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    // 전역으로 동작
+    <QueryClientProvider client={queryClient}>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    </QueryClientProvider>
   );
 }
 
